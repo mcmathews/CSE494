2d00cf728e72d9b03f8b3ee9c27463eded229a66
Michael Mathews <mcmathe1@asu.edu>
2016-01-15 07:34:26 -0700

    Finish Lab1 Android App

--- Lab1/Android/MovieDescription/app/src/main/java/edu/asu/bscs/mcmathe1/moviedescription/MainActivity.java
+++ Lab1/Android/MovieDescription/app/src/main/java/edu/asu/bscs/mcmathe1/moviedescription/MainActivity.java
@@ -1,16 +1,46 @@
 package edu.asu.bscs.mcmathe1.moviedescription;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
-public class MainActivity extends ActionBarActivity {
+/**
+ * Copyright 2015 Michael Mathews
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author Michael Mathews    mailto:Michael.C.Mathews@asu.edu
+ * @version 1/14/2016
+ */
+public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		MovieDescription md = new MovieDescription("{\"Title\":\"The Dark Knight\",\"Year\":\"2008\",\"Rated\":\"PG-13\",\"Released\":\"18 Jul 2008\",\"Runtime\":\"152 min\",\"Genre\":\"Action, Crime, Drama\",\"Director\":\"Christopher Nolan\",\"Writer\":\"Jonathan Nolan (screenplay), Christopher Nolan (screenplay), Christopher Nolan (story), David S. Goyer (story), Bob Kane (characters)\",\"Actors\":\"Christian Bale, Heath Ledger, Aaron Eckhart, Michael Caine\",\"Plot\":\"When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, the caped crusader must come to terms with one of the greatest psychological tests of his ability to fight injustice.\",\"Language\":\"English, Mandarin\",\"Country\":\"USA, UK\",\"Awards\":\"Won 2 Oscars. Another 141 wins & 126 nominations.\",\"Poster\":\"http://ia.media-imdb.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SX300.jpg\",\"Metascore\":\"82\",\"imdbRating\":\"9.0\",\"imdbVotes\":\"1,564,829\",\"imdbID\":\"tt0468569\",\"Type\":\"movie\",\"Response\":\"True\"}");
+		((TextView) findViewById(R.id.titleView)).append(md.getTitle());
+		((TextView) findViewById(R.id.yearView)).append(Integer.toString(md.getYear()));
+		((TextView) findViewById(R.id.ratedView)).append(md.getRated());
+		((TextView) findViewById(R.id.releasedView)).append(md.getReleased());
+		((TextView) findViewById(R.id.runtimeView)).append(md.getRuntime());
+		((TextView) findViewById(R.id.genreView)).append(md.getGenre());
+		((TextView) findViewById(R.id.actorsView)).append(md.getActors());
+		((TextView) findViewById(R.id.plotView)).append(md.getPlot());
+		((TextView) findViewById(R.id.jsonView)).append(md.toJsonString());
 	}
 
 	@Override
---
+++ Lab1/Android/MovieDescription/app/src/main/java/edu/asu/bscs/mcmathe1/moviedescription/MovieDescription.java
@@ -0,0 +1,132 @@
+package edu.asu.bscs.mcmathe1.moviedescription;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Copyright 2015 Michael Mathews
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author Michael Mathews    mailto:Michael.C.Mathews@asu.edu
+ * @version 1/14/2016
+ */
+public class MovieDescription {
+	private String title;
+	private int year;
+	private String rated;
+	private String released;
+	private String runtime;
+	private String genre;
+	private String actors;
+	private String plot;
+
+	public MovieDescription(String json) {
+		try {
+			JSONObject jo = new JSONObject(json);
+			this.title = jo.getString("Title");
+			this.year = Integer.parseInt(jo.getString("Year"));
+			this.rated = jo.getString("Rated");
+			this.released = jo.getString("Released");
+			this.runtime = jo.getString("Runtime");
+			this.genre = jo.getString("Genre");
+			this.actors = jo.getString("Actors");
+			this.plot = jo.getString("Plot");
+
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public String toJsonString() {
+		try {
+			return new JSONObject()
+					       .accumulate("Title", title)
+					       .accumulate("Year", year)
+						   .accumulate("Rated", rated)
+						   .accumulate("Released", released)
+						   .accumulate("Runtime", runtime)
+						   .accumulate("Genre", genre)
+						   .accumulate("Actors", actors)
+						   .accumulate("Plot", plot)
+					       .toString();
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public String getRated() {
+		return rated;
+	}
+
+	public void setRated(String rated) {
+		this.rated = rated;
+	}
+
+	public String getReleased() {
+		return released;
+	}
+
+	public void setReleased(String released) {
+		this.released = released;
+	}
+
+	public String getRuntime() {
+		return runtime;
+	}
+
+	public void setRuntime(String runtime) {
+		this.runtime = runtime;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
+	public String getActors() {
+		return actors;
+	}
+
+	public void setActors(String actors) {
+		this.actors = actors;
+	}
+
+	public String getPlot() {
+		return plot;
+	}
+
+	public void setPlot(String plot) {
+		this.plot = plot;
+	}
+}
