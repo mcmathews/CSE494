b8f8844ffb956fdb0a5f3813f2417e97311e2c00
Michael Mathews <mcmathe1@asu.edu>
2016-01-25 08:20:28 -0700

    Add logging and alert to iOS app for Lab2

--- Lab2/iOS/LifeCycle/LifeCycle/ViewController.swift
+++ Lab2/iOS/LifeCycle/LifeCycle/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func handleButtonPress(sender: UIButton) {
+        print("A thing will be done")
+        
+        let alert = UIAlertController(title: "The thing", message: "This is the thing you wanted to do", preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: "I'm done with the thing", style: UIAlertActionStyle.Default, handler: nil))
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
 
 }
 
