3a1b1912fc66a73fc94a53c3e264b0f2b539b204
Michael Mathews <mcmathe1@asu.edu>
2016-01-26 16:13:22 -0700

    Lab2 Android save point

--- Lab2/Android/LifeCycle/app/src/main/java/edu/asu/bscs/mcmathe1/lifecycle/AlertActivity.java
+++ Lab2/Android/LifeCycle/app/src/main/java/edu/asu/bscs/mcmathe1/lifecycle/AlertActivity.java
@@ -6,6 +6,24 @@ import android.util.Log;
 
 import mcmathe1.bscs.asu.edu.lifecycle.R;
 
+/**
+ * Copyright 2016 Michael Mathews
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author   Michael Mathews    mailto:Michael.C.Mathews@asu.edu
+ * @version Jan 26, 2016
+ */
 public class AlertActivity extends AppCompatActivity {
 
     @Override
@@ -57,6 +75,12 @@ public class AlertActivity extends AppCompatActivity {
 
         logMethodName("onDestroy()");
     }
+
+	protected void handleButtonPress() {
+		Log.w(this.getClass().getSimpleName(), "Ok button pressed");
+
+		finish();
+	}
 
     private void logMethodName(String methodName) {
         Log.w(this.getClass().getSimpleName(), methodName);
--- Lab2/Android/LifeCycle/app/src/main/java/edu/asu/bscs/mcmathe1/lifecycle/MainActivity.java
+++ Lab2/Android/LifeCycle/app/src/main/java/edu/asu/bscs/mcmathe1/lifecycle/MainActivity.java
@@ -7,6 +7,24 @@ import android.util.Log;
 
 import mcmathe1.bscs.asu.edu.lifecycle.R;
 
+/**
+ * Copyright 2016 Michael Mathews
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author   Michael Mathews    mailto:Michael.C.Mathews@asu.edu
+ * @version Jan 26, 2016
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -60,9 +78,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     protected void handleButtonPress() {
-        Log.w("test", "test");
+        Log.w(this.getClass().getSimpleName(), "Button pressed");
 
-        this.startActivity(new Intent(this, AlertActivity.class));
+        startActivity(new Intent(this, AlertActivity.class));
     }
 
     private void logMethodName(String methodName) {
